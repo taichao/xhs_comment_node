f3b9a7db120595651305d1d53d6267c219be1ed0
wuhui <wuhui@shinc.net>
2015-08-19 14:58:01 +0800

    comment

--- controllers/xhs/comment.js
+++ controllers/xhs/comment.js
@@ -9,7 +9,7 @@ var remoteRequest = require('libs/remoteRequest');
 /* 通用评论列表 */
 router.get('/common_comment_list', function(req, res, next) {
 
-	res.render('xhs/comment/article_comment_list');  	
+	res.render('xhs/comment/common_comment_list');  	
 });
 
 router.get('/comment', function(req, res, next) {
