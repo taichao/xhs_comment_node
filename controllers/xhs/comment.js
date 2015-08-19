cf1a4139a20afd7a90b9b00d18d58267ee859ff9
wuhui <wuhui@shinc.net>
2015-08-19 18:57:30 +0800

    select

--- controllers/xhs/comment.js
+++ controllers/xhs/comment.js
@@ -6,15 +6,32 @@ var express = require('express');
 var router = express.Router();
 var remoteRequest = require('libs/remoteRequest');
 
-/* 通用评论列表 */
-router.get('/common_comment_list', function(req, res, next) {
+/* 发布评论列表 */
+router.get('/release_comment_list', function(req, res, next) {
 
-	res.render('xhs/comment/common_comment_list');  	
+	res.render('xhs/comment/release_comment_list');  	
 });
 
 router.get('/comment', function(req, res, next) {
 
 	res.render('xhs/comment/comment');  	
+});
+
+router.get('/comment_category' , function(req , res , next){
+	var baseRequest = remoteRequest(req , res);
+
+	var url = '/basecomment/getCategory';
+
+	var data = req.body;
+
+	baseRequest.post(url , data , function(err , response , body){
+		var jsonStr = JSON.parse(body);
+		if('SUCCESS' == jsonStr.code){
+			res.end(JSON.stringify(jsonStr.result))
+		}else{
+			res.redirect('/login');
+		}
+	});	
 });
 
 module.exports=router
